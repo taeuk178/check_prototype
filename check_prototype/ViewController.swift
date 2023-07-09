844c7c22ea03fa60bdd2dca981e648baec9489b6
taeuk178 <nous178@naver.com>
2023-07-09 22:06:53 +0900

    Initial Commit

---
+++ check_prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  check_prototype
+//
+//  Created by tw on 2023/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
